e35a4276628f4c2e31ca0fea35f6b40c7d747689
Shree Prasad M <sprasad2804@gmail.com>
2023-08-25 07:41:00 +0000

    ...

--- Graph/Lab 2/Kruskal.py
+++ Graph/Lab 2/Kruskal.py
@@ -25,12 +25,6 @@ def kruskal(graph):
 
 
 if __name__ == '__main__':
-    graph_dict = {  "s1":{"s1": 0, "s2": 6, "s10": 3, "s12": 4, "s5":3},
-                    "s2":{"s1": 1, "s2": 0, "s10": 4, "s12": 3, "s5":4},
-                    "s10":{"s1": 2, "s2": 6, "s10": 0, "s12":3, "s5":4},
-                    "s12":{"s1": 1, "s2": 5, "s10": 2, "s12":0,"s5":2},
-                    "s5":{"s1": 3, "s2": 5, "s10": 7, "s12":4,"s5":0},
-    }
-
+    graph_dict = #Graph input
     path = kruskal(graph_dict)
     print path
