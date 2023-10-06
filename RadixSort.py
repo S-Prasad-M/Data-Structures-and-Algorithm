c949ef382d303752276f5eb36ce39401738e9ca9
Shree Prasad M <sprasad2804@gmail.com>
2023-10-06 08:25:09 +0000

    ...

--- RadixSort.py
+++ RadixSort.py
@@ -2,8 +2,8 @@ L = []
 # n = int(input("Ënter number of elements:"))
 # for i in range(n):
 #     L.append(input("Ënter:"))
-L = ['23','12','56','02','15']
+L = ['323','512','156','102','115']
 # print(L)
-for i in range(1,-1,-1):
+for i in range(2,-1,-1):
     L.sort(key=lambda x: x[i])
 print(L)
