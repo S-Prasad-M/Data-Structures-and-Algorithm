e5c2390f40bf97abace567c3a019ef076f72d230
Shree Prasad M <sprasad2804@gmail.com>
2023-10-06 08:18:06 +0000

    ...

--- RadixSort.py
+++ RadixSort.py
@@ -1,2 +1,9 @@
 L = []
-n = int(input("Ënter number of elements:"))
+# n = int(input("Ënter number of elements:"))
+# for i in range(n):
+#     L.append(input("Ënter:"))
+L = ['23','12','56','02','15']
+# print(L)
+for i in range(1,-1,-1):
+    L.sort(key=lambda x: x[i])
+print(L)
