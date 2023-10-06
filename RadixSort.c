5db826e02d347e81431abee46654c32309a7f854
Shree Prasad M <sprasad2804@gmail.com>
2023-10-06 09:44:06 +0000

    ........

--- RadixSort.c
+++
@@ -1,3 +0,0 @@
-#include<stdio.h>
-#include<conio.h>
-int arr[5] ={234,123,567,34,345}
--- RadixSort.py
+++ RadixSort.py
@@ -1,9 +1,23 @@
-L = []
-# n = int(input("Ënter number of elements:"))
-# for i in range(n):
-#     L.append(input("Ënter:"))
-L = ['323','512','156','102','115']
+#  Using String testing sample. Useless code
+# L = []
+# # n = int(input("Ënter number of elements:"))
+# # for i in range(n):
+# #     L.append(input("Ënter:"))
+# L = ['323','512','156','102','115']
+# # print(L)
+# for i in range(2,-1,-1):
+#     L.sort(key=lambda x: x[i])
 # print(L)
-for i in range(2,-1,-1):
-    L.sort(key=lambda x: x[i])
-print(L)
+
+d = {0:[],1:[],2:[],3:[],4:[],5:[],6:[],7:[],8:[],9:[]}
+n = int(input("Ënter number of elements:"))
+for i in range(n):
+    L.append(input("Ënter:"))
+m = max(L)
+mod=10
+for i in range(len(str(m))):
+    for j in L:
+        d[int((j%mod)/(10*i)].append(j)
+        
+    
+
