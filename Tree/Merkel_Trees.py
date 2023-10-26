6504e7cec4758e819ca96060b7e85fa1493932d1
Shree Prasad M <sprasad2804@gmail.com>
2023-10-26 22:29:34 +0530

    Update Merkel_Trees.py

--- Tree/Merkel_Trees.py
+++ Tree/Merkel_Trees.py
@@ -46,3 +46,26 @@ print("Merkle Root:", fullTree[-1][0])  # Merkle Root is the last element
 # Print the full tree
 print("\nFull Merkle Tree:")
 printMerkleTree(fullTree)
+
+
+# import hashlib
+# def build_merkle_tree(leaves):
+#     if len(leaves) == 0:
+#         return None
+#     if len(leaves) == 1:
+#         return leaves[0]
+#     new_leaves = []
+#     for i in range(0, len(leaves), 2):
+#         data = leaves[i]
+#         if i + 1 < len(leaves):
+#             data += leaves[i + 1]
+#         new_leaves.append(hash_data(data))
+#     return build_merkle_tree(new_leaves)
+
+# def hash_data(data):
+#     return hashlib.sha256(data.encode()).hexdigest()
+
+# if _name_ == "_main_":
+#     data = ["A", "B", "C", "D"]
+#     merkle_root = build_merkle_tree(data)
+#     print("Merkle Root:", merkle_root)
