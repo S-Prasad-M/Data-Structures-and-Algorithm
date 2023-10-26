14c9b9f47dcda02f8cd156c9e11e0721c786deda
Shree Prasad M <sprasad2804@gmail.com>
2023-10-26 21:14:04 +0530

    Update DFS.py

--- Graph/Lab 1/DFS.py
+++ Graph/Lab 1/DFS.py
@@ -6,5 +6,9 @@ def dfs(graph, node, visited):
     return visited
 
 graph = {1: [2, 4], 2: [1, 3, 4, 5], 3: [2, 5], 4: [1, 2,5], 5:[2, 3, 4]}   # Input Graph as dictionary here
+n = int(input())
+for i in range(1,n+1):
+    print("Enter Adjacent vertices of ",i,"as list:")
+    graph[i] = eval(input())
 visited = dfs(graph,1, [])
 print(visited)
